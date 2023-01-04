a675f01640fc7f69f2afe9ccc6d7fd59b8423f0b
TranNhatMinh <nhatminh10b1@gmail.com>
2023-01-04 07:07:19 +0700

    LandingPage: 04.01.2023

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,42 @@
 // PRELOADER
 var loader = document.getElementById("preloader");
 window.addEventListener("load",function(){
-  loader.style.display = "none";
+  // loader.style.display = "none";
+  loader.style.opacity = "0";
+  loader.style.visibility = "hidden";
 });
+//NEWTECH
+// import 'https:\/\/flackr.github.io/scroll-timeline/dist/scroll-timeline.js';
+// const animatedImage1 = document.querySelector(".newtech-img1");
+// const animatedImage2 = document.querySelector(".newtech-img2");
+// const animatedImageTimeline1 = new ScrollTimeline({
+//   scrollOffsets: [
+//     {target: animatedImage1, edge: "end", threshold:0},
+//     {target: animatedImage1, edge: "start", threshold:0}
+//   ]
+// });
+// const animatedImageTimeline2 = new ScrollTimeline({
+//   scrollOffsets: [
+//     {target: animatedImage2, edge: "end", threshold:0},
+//     {target: animatedImage2, edge: "start", threshold:0}
+//   ]
+// });
+// animatedImage1.animate(
+//   {
+//     transform:["translateY(0px)","translateY(-50px)"]
+//   },
+//   {
+//     duration:1,
+//     timeline: animatedImageTimeline1,
+//   }
+// );
+// animatedImage2.animate(
+//   {
+//     transform:["translateY(0px)","translateY(-100px)"]
+//   },
+//   {
+//     duration:1,
+//     timeline: animatedImageTimeline2,
+//   }
+// );
+
